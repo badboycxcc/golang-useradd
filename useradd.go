d66436b17c8e279582f091d05c6d51e799a473f5
BackDoor <72059221+badboycxcc@users.noreply.github.com>
2022-11-03 13:35:57 +0800

    Update useradd.go

--- useradd.go
+++ useradd.go
@@ -1,18 +1,32 @@
 package main
 
 import (
-  //  "fmt"
-    wapi "github.com/iamacarpet/go-win64api"
+	"flag"
+	"fmt"
+
+	wapi "github.com/iamacarpet/go-win64api"
 )
 
-func main(){
-    username := "testtest"
-    password := "abc.123"
-    fullname := ""
-    groupname := "administrators"
-    groupuser := []string{"testtest"}
-    wapi.UserAdd(username, fullname, password)
-    wapi.LocalGroupAddMembers(groupname,groupuser)
+func main() {
 
-    //users, err := wapi.ListLocalUsers()
+	// 用户
+	var username string
+	// 密码
+	var password string
+	// 管理组
+	var groupname string
+
+	flag.StringVar(&username, "u", "admin123", "用户名,默认 admin123")
+	flag.StringVar(&password, "p", "admin123", "密码,默认 admin123")
+	flag.StringVar(&groupname, "g", "administrators", "用户组,默认 administrators")
+	flag.Parse()
+
+	fullname := ""
+	groupuser := []string{username}
+	wapi.UserAdd(username, fullname, password)
+	wapi.LocalGroupAddMembers(groupname, groupuser)
+
+	// 打印
+	fmt.Printf("Add User username=%v password=%v group=%v\n", username, password, groupname)
+
 }
